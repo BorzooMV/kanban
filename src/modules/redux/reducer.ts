308c736788cc4e999b6a4f627005ac81175179d9
Borzoo Moazami <borzoomoazami94@gmail.com>
2023-01-26 16:18:21 +0330

    fix: remove test codes from redux reducer

--- src/modules/redux/reducer.ts
+++ src/modules/redux/reducer.ts
@@ -1,18 +1,12 @@
-import { TEST_OK } from "./actionTypes";
 import { ReduxActionType } from "../../ts/types";
 
-const INITIAL_STATE = {
-  test: "failed",
-};
+const INITIAL_STATE = {};
 
 export default function reducer(
   state = INITIAL_STATE,
   action: ReduxActionType
 ) {
   switch (action.type) {
-    case TEST_OK:
-      return { ...state, test: "OK" };
-
     default:
       return state;
   }
